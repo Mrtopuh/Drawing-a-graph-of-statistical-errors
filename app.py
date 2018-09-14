04d9ec9754a8246e95fa50622e5519dbc1eef2ec
mrtopuh <42946352+Mrtopuh@users.noreply.github.com>
2018-09-14 22:05:36 +0300

    app.py

--- app.py
+++ app.py
@@ -25,21 +25,21 @@ MODE = str(input()) # can take value: errors, probability_H0_exceed_H1, probabil
 # Сarry out z transformation
 
 
-def z_deter(x, edge, error_edge, MODE):
+def z_deter(value, edge, error_edge, MODE):
     if MODE == 'probability_H0_exceed_H1' or MODE == 'probability_H0_not_exceed_H1':
-        x = (meanValueOfTheGroup - meanValueOfTheHypothesis) / mean_of_global_deviation
+        value = (meanValueOfTheGroup - meanValueOfTheHypothesis) / mean_of_global_deviation
     elif MODE == 'errors':
-        x = (meanValueOfTheGroup - meanValueOfTheHypothesis) / (meanDeviationGroup / np.sqrt(numberOfObjectsGroup))
+        value = (meanValueOfTheGroup - meanValueOfTheHypothesis) / (meanDeviationGroup / np.sqrt(numberOfObjectsGroup))
     else:
         print('Not enough input data')
         exit()
-    if ((x < 0) and (edge > 0)) or ((x > 0) and (edge < 0)):
-        x *= -1
-    if abs(x) >= abs(edge):
-        x = edge
-    elif abs(x) <= abs(error_edge):
-        x = error_edge
-    return x
+    if ((value < 0) and (edge > 0)) or ((value > 0) and (edge < 0)):
+        value *= -1
+    if abs(value) >= abs(edge):
+        value = edge
+    elif abs(value) <= abs(error_edge):
+        value = error_edge
+    return value
 
 
 z = z_deter(z, RIGHT_EDGE, RIGHT_ERR_EDGE, MODE)
@@ -52,8 +52,8 @@ x = np.linspace(LEFT_EDGE, RIGHT_EDGE, point_count)
 y = (1 / (np.sqrt(2 * np.pi))) * (np.exp((-1 * (x ** 2)) / 2))
 
 
-def y_calc(x):
-    return (1 / (np.sqrt(2 * np.pi))) * (np.exp((-1 * (x ** 2)) / 2))
+def y_calc(value):
+    return (1 / (np.sqrt(2 * np.pi))) * (np.exp((-1 * (value ** 2)) / 2))
 
 
 def get_err_interval(x_list, y_list, start, end):
@@ -124,30 +124,29 @@ def calculate_zones(params):
 # The function definition block for drawing
 
 
-def fill_between(zone, color='green'):
-    plt.fill_between(zone['x'], zone['z'], zone['y'], color=color, alpha=0.25)
+def fill_between(zone, colour='green'):
+    plt.fill_between(zone['x'], zone['z'], zone['y'], color=colour, alpha=0.25)
 
 
 def calculation_of_integrals(mode):
     if mode == 'errors':
-        I_first_left = quad(y_calc, LEFT_EDGE, left_z)
-        I_second_left = quad(y_calc, left_z, LEFT_ERR_EDGE)
-        I_first_right = quad(y_calc, z, RIGHT_EDGE)
-        I_second_right = quad(y_calc, RIGHT_ERR_EDGE, z)
-        integral = abs(I_first_left[0]) + abs(I_second_left[0]) + abs(I_first_right[0]) + abs(I_second_right[0])
+        i_first_left = quad(y_calc, LEFT_EDGE, left_z)
+        i_second_left = quad(y_calc, left_z, LEFT_ERR_EDGE)
+        i_first_right = quad(y_calc, z, RIGHT_EDGE)
+        i_second_right = quad(y_calc, RIGHT_ERR_EDGE, z)
+        integral = abs(i_first_left[0]) + abs(i_second_left[0]) + abs(i_first_right[0]) + abs(i_second_right[0])
     elif mode == 'probability_H0_not_exceed_H1':
-        I = quad(y_calc, LEFT_EDGE, z)
-        integral = abs(I[0])
+        integral_number = quad(y_calc, LEFT_EDGE, z)
+        integral = abs(integral_number[0])
     elif mode == 'probability_H0_exceed_H1':
-        I = quad(y_calc, z, RIGHT_EDGE)
-        integral = abs(I[0])
+        integral_number = quad(y_calc, z, RIGHT_EDGE)
+        integral = abs(integral_number[0])
     percent = round(integral * 100, 4)
     s_persent = str(percent) + '%'
     return s_persent
 
 
 def draw(mode):
-    my_dict = {'color': 'green', 'linewidth': 4.0, 'alpha': 0.5}
     plt.plot(x, y, 'r', label='функция нормального распределения')
     plt.xlabel('Признак')
     plt.ylabel('Распределение или частота встречи')
@@ -155,7 +154,6 @@ def draw(mode):
     plt.title("График нормального распределения после преобразования в единицы стандартного отклонения")
     plt.legend(loc='upper left')
     fig.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
-
     if mode == 'errors':
         plt.text(-3.1, 0.05, u"зелёное 1 рода", family="verdana")
         plt.text(2.1, 0.05, u"зелёное 1 рода", family="verdana")
@@ -193,11 +191,8 @@ for i in range(len(zones_list)):
     if (type == 'probability_neg') or (type == 'err_2'):
         color = 'red'
     fill_between(fill_params['zone'], color)
-
-
-
 draw(MODE)
 
 # Output data and save Graph
-plt.savefig(r'C:\Users\mrtop\Desktop\НАЙТИ РАБОТУ В КОДИНГЕ!!!!!\Figure')
+plt.savefig(r'C:\Users\Figure')
 plt.show()
